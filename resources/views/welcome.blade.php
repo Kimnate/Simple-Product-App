663cb6798b9a9927b0d4809a98989b625565b58c
Nathan Chirchir <nathan.chirchir@peptang.com>
2025-01-08 15:07:44 +0300

    Add GitHub Link on Welcome Page 08012025 1500HRS

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,9 +4,22 @@
 
     <div class="welcome-container">
 
-        <h1> Welcome to Simple Products App </h1>
-        <p>Effortlessly manage Products, Prices & Stock Quantities with this lightweight and user-friendly application. Click below to get started!</p>
+        <h1> Welcome to a Simple Product App </h1>
+        <hr>
+
+        <p class="justified-text">Effortlessly manage Products, Prices & Stock Quantities with this lightweight and user-friendly application. Click below to get started!</p>
+
         <a href="{{ route('products.index') }}" class="btn btn-primary"> Get Started </a>
+
+        <hr>
+
+        <div class="">
+            <a href="https://github.com/Kimnate/Simple-Product-App" target="_blank">
+                <i class="bx bxl-github custom-icon"> </i>
+            </a>
+
+        </div>
+        <small> Or check it out on Github </small>
 
     </div>
 
